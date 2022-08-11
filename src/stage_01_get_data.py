c7e94c269db0746d217513ebe95f07f0f11418be
Sarika Mohanraj <sarikamohan08@gmail.com>
2022-08-11 22:12:33 +0530

    get added updated

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,11 +1,12 @@
-import argparse
 import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, unzip_file
+#from src.utils.data_mgmt import validate_image
 import random
 import urllib.request as req
+import argparse
 
 STAGE = "GET_DATA" ## <<< change stage name 
 
@@ -17,34 +18,42 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
     URL = config["data"]["source_url"]
     local_dir = config["data"]["local_dir"]
+    create_directories([local_dir])
+
     data_file = config["data"]["data_file"]
     data_file_path = os.path.join(local_dir, data_file)
 
-    create_directories([local_dir])
     
     if not os.path.isfile(data_file_path):
-        logging.log(f"downloading started....")
+        logging.info("downloading started...")
         filename, headers = req.urlretrieve(URL, data_file_path)
         logging.info(f"filename:{filename} created with info \n{headers}")
     else:
         logging.info(f"filename:{data_file} already present")
 
+    # Unzip ops
+    unzip_data_dir = config["data"]["unzip_data_dir"]
+
+    create_directories([unzip_data_dir])
+    unzip_file(source=data_file_path, dest=unzip_data_dir)
+    
+    # validating data
+    #validate_image(config)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        main(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
